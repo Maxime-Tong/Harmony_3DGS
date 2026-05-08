7380c7333ff40a42f6d96f909323654c3623d152
Xiaotong Huang <74357537+Maxime-Tong@users.noreply.github.com>
2026-05-08 15:54:25 +0800

    Refactor camera view generation functions

--- scripts/generate_video_path.py
+++ scripts/generate_video_path.py
@@ -14,7 +14,7 @@ import numpy as np
 from argparse import ArgumentParser
 import json
 
-from pose_utils import generate_ellipse_path, generate_spherical_sample_path, generate_spiral_path, generate_spherify_path,  gaussian_poses, circular_poses
+from pose_utils import generate_ellipse_path, getWorld2View2
 from pose_utils import View
 
 def load_camera_poses(camera_file):
@@ -22,34 +22,62 @@ def load_camera_poses(camera_file):
         camera_data = json.load(f)
     return camera_data
 
-def generate_views(args):
+def generate_ellipse_views(args):
     camera_data = load_camera_poses(args.camera)
     fx, fy = camera_data[0]['fx'], camera_data[0]['fy']
+    W, H = camera_data[0]['width'], camera_data[0]['height']
     
     views = []
     for pose in camera_data:
-        W2C = np.eye(4)
-        W2C[:3, :3] = np.array(pose['rotation'])
-        W2C[:3, 3] = np.array(pose['position'])
-        C2W = np.linalg.inv(W2C)
-        R = C2W[:3, :3].T
-        T = C2W[:3, 3]
-        
+        R = np.array(pose['rotation']).T
+        T = np.array(pose['position'])
         view = View(R, T)
         views.append(view)
     pose_ellipse = generate_ellipse_path(views, n_frames=100)
     
     camera_infos = []
     for idx, pose in enumerate(pose_ellipse):
-        R = pose[:3, :3]
-        T = pose[:3, 3] 
         camera_info = {
             'id': idx,
             'img_name': f"ellipse_{idx:03d}.png",
-            'width': 720,
-            'height': 1080,
+            'width': W,
+            'height': H,
             'rotation': pose[:3, :3].tolist(),
             'position': pose[:3, 3].tolist(),
+            'fy': fy,
+            'fx': fx
+        }
+        camera_infos.append(camera_info)
+    
+    with open(args.output, 'w') as f:
+        json.dump(camera_infos, f, indent=4)
+        
+    return camera_infos
+
+def generate_circular_views(args, radius=0.5, n_frames=240):
+    camera_data = load_camera_poses(args.camera)
+    fx, fy = camera_data[0]['fx'], camera_data[0]['fy']
+    W, H = camera_data[0]['width'], camera_data[0]['height']
+    
+    camera_infos = []
+    for idx, pose in enumerate(camera_data):
+        R = np.array(pose['rotation']).T
+        T = np.array(pose['position'])
+        
+        angle = 2 * np.pi * idx / n_frames
+        translate_x = radius * np.cos(angle)
+        translate_y = radius * np.sin(angle)
+        translate_z = 0
+        translate = np.array([translate_x, translate_y, translate_z])
+        
+        W2C = getWorld2View2(R, T, translate)
+        camera_info = {
+            'id': idx,
+            'img_name': f"circular_{idx:03d}.png",
+            'width': W,
+            'height': H,
+            'rotation': W2C[:3, :3].tolist(),
+            'position': W2C[:3, 3].tolist(),
             'fy': fy,
             'fx': fx
         }
@@ -68,4 +96,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print("Read Camera " + args.camera)
     
-    generate_views(args)
+    # generate_ellipse_views(args)
+    generate_circular_views(args)
